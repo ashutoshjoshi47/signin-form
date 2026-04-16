ca6750a986b258849d326afd63293fe70e623313
Aashutosh Joshi <125844407+ashutoshjoshi47@users.noreply.github.com>
2026-04-16 12:13:30 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,6 @@ const port = process.env.PORT || 8000
 
 connectDB();
 
-// app.use((req, res, next) => {
-//   console.log("➡️ REQUEST:", req.method, req.url)
-//   next()
-// })
 
 app.use(express.json())
 app.use(cors())
